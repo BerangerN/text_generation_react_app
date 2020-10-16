e4c5936c8ddf06b349ca23e47f5052decdc20701
Beranger <beranger.n@protonmail.com>
2020-10-16 23:44:03 +0200

    Updates

---
+++ precache-manifest.1dd5d73ac3c54c031675d8fcbb6d74e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aba2d4be1ce35284bdca2c7c74c0371",
+    "url": "/text_generation_react_app/index.html"
+  },
+  {
+    "revision": "3fa055784641b2f910c5",
+    "url": "/text_generation_react_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08adc88923db4f0fe46f",
+    "url": "/text_generation_react_app/static/js/2.cd138970.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/text_generation_react_app/static/js/2.cd138970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa055784641b2f910c5",
+    "url": "/text_generation_react_app/static/js/main.1c7bf0b4.chunk.js"
+  },
+  {
+    "revision": "7b58b08dce56dc966798",
+    "url": "/text_generation_react_app/static/js/runtime-main.2a7d3efd.js"
+  }
+]);
